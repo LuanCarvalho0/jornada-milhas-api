0cfee798cb2dfc2067c82d6825b27a8f042b97f9
luancarvalho0 <luan_carvalho1997@hotmail.com>
2023-09-22 20:28:09 -0300

    model depoimento

--- jornada_milhas/models.py
+++ jornada_milhas/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Depoimento(models.Model):
+    foto = models.ImageField()
+    depoimento = models.TextField()
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.depoimento
