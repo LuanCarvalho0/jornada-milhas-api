841a813b03b6b42f76e0b174251296cfade454aa
luancarvalho0 <luan_carvalho1997@hotmail.com>
2023-09-23 16:37:25 -0300

    config media

--- setup/urls.py
+++ setup/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from jornada_milhas.views import DepoimentosViewSet
 from rest_framework import routers
 
@@ -11,4 +13,4 @@ router.register('depoimentos', DepoimentosViewSet, basename='Depoimentos')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
