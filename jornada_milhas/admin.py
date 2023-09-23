0232688d3b54ff49da68af568cb5522475625bb1
luancarvalho0 <luan_carvalho1997@hotmail.com>
2023-09-23 15:05:11 -0300

    admin Depoimentos

--- jornada_milhas/admin.py
+++ jornada_milhas/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from jornada_milhas.models import Depoimento
 
-# Register your models here.
+
+class Depoimentos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'depoimento', 'foto')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome',)
+
+admin.site.register(Depoimento, Depoimentos)
