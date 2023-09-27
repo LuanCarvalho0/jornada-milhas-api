1681b6deb336c0b0e1a88a7a54cfe6bd970ae20f
luancarvalho0 <luan_carvalho1997@hotmail.com>
2023-09-27 19:06:54 -0300

    Update

--- jornada_milhas/views.py
+++ jornada_milhas/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets
 from jornada_milhas.serializer import DepoimentoSerializer
 from jornada_milhas.models import Depoimento
 from rest_framework.response import Response
@@ -11,14 +11,14 @@ class DepoimentosViewSet(viewsets.ModelViewSet):
     
     def update(self, request, *args, **kwargs):
         db_depoimento = self.get_object()
-        if not db_depoimento == None:
+        if db_depoimento:
             db_depoimento.remove_foto()
 
         return super().update(request, *args, **kwargs)
 
     def destroy(self, request, *args, **kwargs):
         db_depoimento = self.get_object()
-        if not db_depoimento == None:
+        if db_depoimento:
             db_depoimento.remove_foto()
         
         return super().destroy(request, *args, **kwargs)
