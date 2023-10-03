b045359a78f35be8d1ce31719595a512dfe4cdd9
luancarvalho0 <luan_carvalho1997@hotmail.com>
2023-10-03 18:57:02 -0300

    delete image with signals

--- jornada_milhas/apps.py
+++ jornada_milhas/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class JornadaMilhasConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'jornada_milhas'
+
+    def ready(self) -> None:
+        from django.db.models.signals import pre_delete, pre_save
+
+        from .models import Depoimento
+        from .signals import delete_image
+
+        pre_delete.connect(delete_image, sender=Depoimento)
+        pre_save.connect(delete_image, sender=Depoimento)
---
+++ jornada_milhas/signals.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+import os
+
+
+def delete_image(sender, instance, **kwargs):
+    depoimento = sender.objects.filter(id=instance.id).first()
+    if depoimento:
+        old_image_path = os.path.join(settings.MEDIA_ROOT, instance.foto.name)
+        if os.path.exists(old_image_path):
+            os.remove(old_image_path)
