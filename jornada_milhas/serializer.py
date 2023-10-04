a6b05a2170a14342b25c80095f8925a7e6a89b49
luancarvalho0 <luan_carvalho1997@hotmail.com>
2023-10-04 18:42:07 -0300

    update delete image

--- jornada_milhas/serializer.py
+++ jornada_milhas/serializer.py
@@ -8,18 +8,3 @@ class DepoimentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Depoimento
         fields = '__all__'
-
-    def update(self, instance, validated_data):
-        if instance.foto:
-
-            old_image_path = instance.foto.path
-
-            if os.path.exists(old_image_path):
-                os.remove(old_image_path)
-
-        instance.nome = validated_data.get('nome', instance.nome)
-        instance.depoimento = validated_data.get('depoimento', instance.depoimento)
-        instance.foto = validated_data.get('foto', instance.foto)
-        instance.save()
-
-        return instance
--- jornada_milhas/signals.py
+++ jornada_milhas/signals.py
@@ -5,6 +5,6 @@ import os
 def delete_image(sender, instance, **kwargs):
     depoimento = sender.objects.filter(id=instance.id).first()
     if depoimento:
-        old_image_path = os.path.join(settings.MEDIA_ROOT, instance.foto.name)
+        old_image_path = os.path.join(settings.MEDIA_ROOT, depoimento.foto.name)
         if os.path.exists(old_image_path):
             os.remove(old_image_path)
