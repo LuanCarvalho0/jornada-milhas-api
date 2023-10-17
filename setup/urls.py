d10c30d8c9f0955d449dadac21596ca7e6a870fa
luancarvalho0 <luan_carvalho1997@hotmail.com>
2023-10-17 18:39:21 -0300

    register router Destino

--- setup/urls.py
+++ setup/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from jornada_milhas.views import DepoimentosViewSet, DepoimentosHomeViewSet
+from jornada_milhas.views import DepoimentosViewSet, DepoimentosHomeViewSet, DestinosViewSet
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
 router.register('depoimentos', DepoimentosViewSet, basename='Depoimentos')
 router.register('depoimentos-home', DepoimentosHomeViewSet, basename='Depoimentos-home')
+router.register('destinos', DestinosViewSet, basename='Destinos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
