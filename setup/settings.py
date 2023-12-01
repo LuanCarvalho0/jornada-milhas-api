64548b6d1fea6de2a9053241d55061cd51e673f5
luancarvalho0 <luan_carvalho1997@hotmail.com>
2023-12-01 20:51:19 -0300

    Integrando o DRF ao Swagger para gerar documentação da API

--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'jornada_milhas',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,26 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from jornada_milhas.views import DepoimentosViewSet, DepoimentosHomeViewSet, DestinosViewSet
 from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Jornada Milhas",
+      default_version='v1',
+      description="É uma API REST que disponibiliza possíveis destinos de viagem.",
+      terms_of_service="#",
+      contact=openapi.Contact(email="luan_carvalho1997@hotmail.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 router = routers.DefaultRouter()
 router.register('depoimentos', DepoimentosViewSet, basename='Depoimentos')
@@ -14,5 +29,6 @@ router.register('destinos', DestinosViewSet, basename='Destinos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
